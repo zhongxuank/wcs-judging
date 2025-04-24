21072d7c0ce6115c177f9c122d8d02d5c563f85c
zhongxuank <zxkhwa@hotmail.com>
2025-04-24 08:34:32 +0000

    Deploying to gh-pages from @ zhongxuank/wcs-judging@d9e367e0d2e11358257e471baa855e2fedcca86b 🚀

--- public/config.js
+++
@@ -1,8 +0,0 @@
-window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyDbmZ-61paEQO4FBpDbdbyVz_lGCTdF4G8",
-  authDomain: "wcs-judging.firebaseapp.com",
-  projectId: "wcs-judging",
-  storageBucket: "wcs-judging.firebasestorage.app",
-  messagingSenderId: "1007715144963",
-  appId: "1:1007715144963:web:ddcf6de424bdc217c2c536"
-}; 

78cbae727e5e4c1e3ae40b1eb7a65419b4177039
Zhong Xuan Khwa <zhongxuan.khwa@grabtaxi.com>
2025-04-24 14:27:23 +0800

    Trigger new deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// Trigger new deployment with environment variables
 export default defineConfig({
   plugins: [react()],
   base: '/wcs-judging/',
